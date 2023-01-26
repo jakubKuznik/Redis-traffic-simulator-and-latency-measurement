474e6165ba6f2959f2c39737cd247df93f966b45
jakubKuznik <kuznja16@gmail.com>
2023-01-26 17:35:59 +0100

    Statistic update

--- redis-latency-measure/redis-latency.cc
+++ redis-latency-measure/redis-latency.cc
@@ -12,11 +12,15 @@
 #include <chrono>
 #include <hiredis/hiredis.h>
 #include <unistd.h> 
-
+#include <fstream>
+ 	   
+ 
 using namespace std;
 
-#define WAIT_SECONDS 0.1 // WAIT 0.1 BETWEEN EACH LATENCY MEASURE 
 #define GETS_P_S 6000    // how many gets will be there p.s 
+#define APPEND_ONLY_FILE "/opt/pcf/pdp-redis/log/appendonly.aof"
+#define PDP_DUMP "/opt/pcf/pdp-redis/log/pdp-dump.rdb"
+#define REDIS_LOG "/opt/pcf/pdp-redis/log/pdp-redis.log"
 
 // Connect to redis database
 // If error exit program 
@@ -69,51 +73,131 @@ void getKey(FILE *f, char *key){
   }
 } 
 
+// count latency for GET
+//   @context ... redis database context 
+//   @key      ... one redis database key  
+long long latencyMeasure(redisContext *context, char *key){
+  
+  redisReply *reply;
+  // start time measure 
+  auto timeStart = chrono::high_resolution_clock::now();  
+    
+  // redis get record 
+  reply = (redisReply*)redisCommand(context, "GET %s",key);
+    
+  // end time measure 
+  auto timeEnd = chrono::high_resolution_clock::now();
+  long long duration = std::chrono::duration_cast<std::chrono::microseconds> (timeEnd - timeStart).count();
+  
+  freeReplyObject(reply); 
+  return duration;
+}
+
+// increment and count proper values 
+void statistic(int latency, int *tQ, int *tQS, long long *tL, long long *tLIS, int *m, int *mS){
+  // increace all counters 
+  (*tQ)++;
+  (*tQS)++;
+  (*tL)   += latency;
+  (*tLIS) += latency;  
+
+  // if maximum set 
+  if (latency > *mS)
+    *mS = latency;
+  if (latency > *m)
+    *m = latency;
+}
+
+// count avg values 
+void countAvg(int tQ, int tQS, long long tL, long long tLIS, int *avg, int *avgS){
+  *avg  = (int) (tL / tQ);
+  *avgS = (int) (tLIS / tQS);  
+}
+
+// set all params to 0 
+void resetCounters(long long *tLIS, int *tQS  ,int *mS){
+  *tLIS = 0;
+  *tQS  = 0;
+  *mS   = 0;
+}
+
+// return string with current time 
+char *currentTime(time_t cTime){
+  char *t = ctime(&cTime);
+  if ( t[strlen(t)-1] == '\n') 
+    t[strlen(t)-1] = '\0';
+  return t;
+}
+
+// return size of file
+int fileSize(char *fileName){
+   ifstream in_file(fileName, ios::binary);
+   in_file.seekg(0, ios::end);
+   return in_file.tellg();
+}
 
 int main(int argc, char **argv){   
-  
+  // declaring argument of time()
+  time_t cTime = time(NULL);  
+  time_t prevTime = cTime; 
+
   // redis object that represent database  
   redisContext *context = connectToRD("localhost", 6379);
 
   // file from arguments with all the key values 
   FILE *keys = parseArgs(argc, argv); 
  
-  redisReply *reply;
-  int indication 	= 0;
-  int measure 		= GETS_P_S * WAIT_SECONDS; // how often there will be a latency measure 
-  float timeWait 	= 1 / GETS_P_S;            // convert gets p. s. to interval 
-  int measureCount 	= 0;			   // latency indicator 
+  long long latency         = 0; 
+  int indication            = 0;
+  float timeWait 	    = 1 / GETS_P_S; // convert gets p. s. to interval 
+
+  int totalQuerries         = 0;	// total querriees  
+  int totalQuerriesSec      = 0;  	// total querries in one second 
+  long long totalLatency    = 0; 	// total latency  
+  long long totalLatencyIS  = 0; 	// total latency in second 
+  int avg	            = 0;	// avg total 
+  int avgS	            = 0;	// avg in second
+  int max 		    = 0;  	// max total
+  int maxS 		    = 0;  	// max in one second  
+
+  char key[20]; 			// redis database key 
   
-  char key[20]; // redis database key 
-  
-  // every 0.1 second count latency and write it to stdout 
+
+  // count latency and write it to stdout 
+  // 26.1. 15:20:21  6000msg avg=1ms max=5ms avgStart=2ms maxStart=100ms 
+  // 		appendonlyAof=111 pdpDumpRdb=111 pdpRedisLog=111 
   while (true){ 
     // get key from redis database 
     getKey(keys, key);
-
-    // count latency each WAIT_SECONS      
-    if (measureCount++ % measure == 0){
-      // start time measure 
-      auto timeStart = chrono::high_resolution_clock::now();  
+    // update time 
+    cTime = time(NULL);  
     
-      // redis get record 
-      redisAppendCommand(context,"GET %s",key);
-      redisGetReply(context,(void**)&reply); // reply for SET
-    
-      // end time measure 
-      auto timeEnd = chrono::high_resolution_clock::now();
-      long long duration = std::chrono::duration_cast<std::chrono::microseconds> (timeEnd - timeStart).count();
- 
-      // log latency in microseconds 
-      cout << duration << endl;  
+    // print values each second 
+    if (prevTime != cTime){
+      // count avg times 
+      countAvg(totalQuerries, totalQuerriesSec, totalLatency, totalLatencyIS, &avg, &avgS); 
+      
+      cout << currentTime(cTime) << " TQS=" << totalQuerriesSec 
+           << " avg=" << avgS << " max=" << maxS << " avgStart=" << avg 
+           << " maxStart=" << max << " appendOnlyAof=" << fileSize(APPEND_ONLY_FILE)
+	   << " pdpDumpRdb=" << fileSize(PDP_DUMP) << " pdpRedisLog=" << fileSize(REDIS_LOG)  << endl;
+      
+      resetCounters(&totalLatencyIS, &totalQuerriesSec, &maxS);      
     }
+    prevTime = cTime;
     
+    // count latency for GET 
+    latency = latencyMeasure(context, key);
+
+    // increment all necessery numbers for statistic 
+    statistic(latency, &totalQuerries, &totalQuerriesSec, &totalLatency, &totalLatencyIS, &max, &maxS); 
+   
     // liveness indicator 
     if (indication++ % 100000 == 0) 
-    	cerr << ".";
+      cerr << ".";
     
     // do GETS_P_S gets each second  
-    usleep((timeWait*1000000));
+    usleep((unsigned int )(timeWait*1000000 ));
   }  
 
   redisFree(context);
